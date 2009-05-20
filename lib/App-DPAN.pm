88564d58fc7f5f0c462447ef18fea806764db493
bfoy <bfoy@WWD-BFOY01.US.DRWHoldings.com>
2009-05-20 15:08:14 -0500

    * Turn of organize_dists by default. People should have to ask to move files around

--- lib/App-DPAN.pm
+++ lib/App-DPAN.pm
@@ -22,7 +22,7 @@ my %Defaults = (
 	indexer_class         => 'MyCPAN::App::Indexer::DPAN',
 	reporter_class        => 'MyCPAN::App::Indexer::DPAN',
 	parallel_jobs         => 1,
-	organize_dists        => 1,
+	organize_dists        => 0,
 	pause_id              => 'DPAN',
 	);
 
