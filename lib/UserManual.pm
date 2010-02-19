f7250fc823f48c93ffd84822e046a14b6dd64cdb
brian d foy <brian.d.foy@gmail.com>
2010-02-18 18:09:24 -0800

    * Stub for user manual

---
+++ lib/UserManual.pm
@@ -0,0 +1,39 @@
+package MyCPAN::Indexer::UserManual;
+use strict;
+use warnings;
+use utf8;
+
+use vars qw($VERSION);
+$VERSION = '1.28_08';
+
+=head1 NAME
+
+MyCPAN::App::DPAN::UserManual - How to manage a DPAN
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+MyCPAN::Indexer::Tutorial
+
+=head1 SOURCE AVAILABILITY
+
+This code is in Github:
+
+	git://github.com/briandfoy/mycpan-indexer.git
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2010, brian d foy, All Rights Reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
